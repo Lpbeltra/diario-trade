8a22e4774360518b508f3d5647e65cc81b1a33df
Lpbeltra <truelisandropb@gmail.com>
2023-01-10 23:06:43 -0300

    Includding php and some html adjustments

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+$ativo = addslashes($_POST['ativo'])
+$data = addslashes($_POST['dataTrade'])
+$hora = addslashes($_POST['horaTrade'])
+$ponta = addslashes($_POST['pontaTrade'])
+$resultado = addslashes($_POST['resultadoTrade'])
+$pontos = addslashes($_POST['pontosTrade'])
+$resultadovalor = addslashes($_POST['valorTrade'])
+$padrao = addslashes($_POST['padraoTrade'])
+$comentarioTrade = addslashes($_POST['comentarioTrade'])
+
+
+
+
+
+
+
+
+
+
+>
