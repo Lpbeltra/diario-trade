114c376a0949d9e2c8c3f410af330293ddf666b1
Lpbeltra <77076216+Lpbeltra@users.noreply.github.com>
2023-01-20 11:44:44 -0300

    Pagina dashboard
    Dashboard em construção

---
+++ dashboard.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trading Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container" style="margin-top: 40px;">
+        <h3>Dashboard</h3>
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Ativo</th>
+                <th scope="col">Data</th>
+                <th scope="col">Ponta</th>
+                <th scope="col">Resultado</th>
+                <th scope="col">Pontos</th>
+                <th scope="col">Resultado R$</th>
+                <th scope="col">Padrão</th>
+                <th scope="col">Comentário</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+              
+            </tbody>
+        </table>
+
+    </div>
+    
+</body>
+</html>
